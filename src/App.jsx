fb382eafeb7bc6859bb0170dc2a18de069b5d766
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-09-28 15:29:02 +0530

    successfully connect react-redux toolkit with application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Quiz from './components/Quiz';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <Quiz />
-    </div>
+    </Provider>
   );
 };
 
